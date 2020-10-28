48967750c046b736001d24fffc526710161956ff
bréval <breval2000@live.fr>
2020-10-28 15:25:37 +0100

    Building query for Accept and Pending

--- src/Controller/DebtController.php
+++ src/Controller/DebtController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Debt;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,12 +10,28 @@ use Symfony\Component\Routing\Annotation\Route;
 class DebtController extends AbstractController
 {
     /**
-     * @Route("/debt", name="debt")
+     * @Route("/pending", name="pending")
      */
     public function index(): Response
     {
+
+
+
+
+
+        $pending  = $this->getDoctrine()
+            ->getRepository(Debt::class)
+            ->findByNotFinished($this->getUser()->getId());
+
+
+
+
         return $this->render('debt/index.html.twig', [
-            'controller_name' => 'DebtController',
+
+            'pending' => $pending,
+
         ]);
+
     }
+
 }
