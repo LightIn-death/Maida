7a8d9b878c21954bd01990f3f02914909ff06120
bréval <breval2000@live.fr>
2020-11-02 22:40:15 +0100

    Remboursement

--- src/Controller/DebtController.php
+++ src/Controller/DebtController.php
@@ -4,6 +4,9 @@ namespace App\Controller;
 
 use App\Entity\Debt;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -29,20 +32,54 @@ class DebtController extends AbstractController
 
 
     /**
-     * @Route("/pending/details/{id}", methods={"GET","HEAD"}, name="details")
+     * @Route("/pending/details/{id}", methods={"GET","POST"}, name="details")
      * @param int $id
+     * @param Request $request
      * @return Response
      */
-    public function details(int $id): Response
+    public function details(int $id ,Request $request): Response
     {
+
+
+
 
         $details  = $this->getDoctrine()
             ->getRepository(Debt::class)
             ->find($id);
 
+
+        $form = $this->createFormBuilder()
+            ->add('newAmount', NumberType::class ,['label' => "Ajouter de l'argent   :"])
+            ->add('save', SubmitType::class, ['label' => 'Ajouter'])
+            ->getForm();
+
+        $form->handleRequest($request);
+
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+
+            $em = $this->getDoctrine()->getManager();
+            $data = $form->getData();
+
+            $newAmount = $data['newAmount'] <  $details->getAmount() -  $details->getAlreadyRefund() ?  $details->getAlreadyRefund() + $data['newAmount'] : $details->getAlreadyRefund() + ($details->getAmount() -  $details->getAlreadyRefund());
+
+
+            $details->setAlreadyRefund($newAmount);
+
+            $em->persist($details);
+
+            $em->flush();
+
+        }
+
+
+
         return $this->render('debt/details.html.twig', [
 
             'details' => $details,
+            'formulaire' => $form->createView(),
+
 
         ]);
 
